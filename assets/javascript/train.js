765809dab6045e957b44f999da4b320dbfe4f251
Jeffrey Davis <davisj0214@yahoo.com>
2019-07-25 21:18:34 -0500

    project complete

---
+++ assets/javascript/train.js
@@ -0,0 +1,72 @@
+
+
+    // ===== FIREBASE CONFIG =====
+    var firebaseConfig = {
+        apiKey: "AIzaSyAn4WC-A3VKbyjRMIIJZdMXditFPlXPqYI",
+        authDomain: "train-class.firebaseapp.com",
+        databaseURL: "https://train-class.firebaseio.com",
+        projectId: "train-class",
+        storageBucket: "",
+        messagingSenderId: "392413794475",
+        appId: "1:392413794475:web:edff0babe145c13f"
+    };
+    // ===== Initialize Firebase =====
+    firebase.initializeApp(firebaseConfig);
+
+    // ===== Variable for database reference =====
+    var database = firebase.database();
+
+    // ===== Global Variables =====
+    var name;
+    var destination;
+    var firstTrainTime;
+    var frequency = 0;
+
+    // ===== CLICK HANDLER FOR SUBMIT BUTTON =====
+    $("#submit-button").on("click", function() {
+
+        event.preventDefault();
+        // ===== Getting data from the user's input =====
+        name = $("#train-name-input").val().trim();
+        destination = $("#destination-input").val().trim();
+        firstTrainTime = $("#train-time-input").val().trim();
+        frequency = $("#frequency-input").val().trim();
+
+        // ===== Pushing the data to the database =====
+        database.ref().push( {
+            name: name,
+            destination: destination,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+        })
+        $("form")[0].reset();
+    })
+
+    database.ref().on("child_added", function(snapshot) {
+
+        // ===== Changing the first train time to reflect before current time =====
+        var firstTrainNew = moment(snapshot.val().firstTrainTime, "HH:mm").subtract(1, "days");
+        // ===== Difference between current time and firstTrainTime =====
+        var difference = moment().diff(moment(firstTrainNew), "minutes");
+        var remainder = difference % snapshot.val().frequency;
+        // ===== Minutes until the next train =====
+        var minutesAway = snapshot.val().frequency - remainder;
+        // ===== Next train arrival =====
+        var nextTrain = moment().add(minutesAway, "minutes");
+        // ===== Formatting the time =====
+        nextTrain = moment(nextTrain).format("HH:mm");
+
+        // ===== APPENDING THE DATA TO THE TABLE =====
+        $("#add-train-info").append("<tr><td>" + snapshot.val().name +
+            "</td><td>" + snapshot.val().destination +
+            "</td><td>" + snapshot.val().frequency +
+            "</td><td>" + nextTrain +
+            "</td><td>" + minutesAway + "</td></tr>");
+
+    }, function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+    })
+
+
+
+
